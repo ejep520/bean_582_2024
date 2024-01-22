8dcd2874865de0123c78714eca1437656c03fc79
Bryan Lipscy <bryan.lipscy@zimmerbiomet.com>
2024-01-22 11:46:37 -0800

    Setting up project skeleton

---
+++ ApartmentFinder/src/main/java/edu/wsu/bean_582_2024/ApartmentFinder/ApartmentFinderApplication.java
@@ -0,0 +1,13 @@
+package edu.wsu.bean_582_2024.ApartmentFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentFinderApplication.class, args);
+	}
+
+}
---
+++ ApartmentFinder/src/test/java/edu/wsu/bean_582_2024/ApartmentFinder/ApartmentFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wsu.bean_582_2024.ApartmentFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
